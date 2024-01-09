b838550390fa3d866cae8b196108b28d4fba5666
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-01-09 22:50:20 +0530

    solved few easy level 500 rating problems

---
+++ Jan9/kitchenTimings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int start,end;
+	    cin>>start>>end;
+	    cout << end-start<<endl;
+    }
+	
+	return 0;
+}
