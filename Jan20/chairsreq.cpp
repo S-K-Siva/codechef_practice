1912c0412345a80555f112a08c0f5c6ec6f48ed9
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-01-20 23:59:49 +0530

    solved 1 easy

---
+++ Jan20/chairsreq.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x-y <= 0) cout << "0" << endl;
+	    else cout << x-y << endl;
+	}
+	return 0;
+}
