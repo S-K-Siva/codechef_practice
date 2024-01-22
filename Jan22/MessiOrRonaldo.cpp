72c37d191b13f0e7fa2cfc119c36a3aa22995784
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-01-22 21:04:08 +0530

    solved below 500 level problems

---
+++ Jan22/MessiOrRonaldo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	if((2*a + b) > (2*x + y)){
+	    cout << "Messi" << endl;
+	}else if((2*a + b) == (2*x + y)){
+	    cout << "Equal" << endl;
+	}else{
+	    cout << "Ronaldo" << endl;
+	}
+	return 0;
+}
