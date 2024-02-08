170c10c137bed385f78cb12a60b3c09d045e9eb3
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-02-08 23:24:32 +0530

    solved one below 500 lvl problem

---
+++ 8Feb24/chefAndWireframes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    
+	    cout << ((2*x)+(2*y))*z << endl;
+	}
+	return 0;
+}
