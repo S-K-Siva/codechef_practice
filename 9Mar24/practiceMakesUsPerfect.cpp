7d67cca5f9ba7776c1d30c543329896f788a184a
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-03-09 22:49:57 +0530

    solved 1 cakewalk question

---
+++ 9Mar24/practiceMakesUsPerfect.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	
+	int cnt = 0;
+	
+	if(a>=10)cnt++;
+	if(b>=10)cnt++;
+	if(c>=10)cnt++;
+	if(d>=10)cnt++;
+	
+	cout << cnt << endl;
+	return 0;
+}
