ea881dc6693bbdf3bf8eb27ec90b48d38964ba1e
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-04-01 22:56:31 +0530

    solved one cakewalk problem

---
+++ 1Apr24/JanmanshAndAssignments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if((10-x) >= 3) cout << "Yes"<<endl;
+        else cout << "No" << endl;
+    }
+	return 0;
+}
