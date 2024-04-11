f9495b3635168b5ccdcedbdc75e3b894f471b298
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-04-11 22:31:49 +0530

    solved potd

---
+++ 11Apr24/cheflandGames.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(a|| b || c || d) cout << "OUT" << endl;
+	    else cout << "IN" << endl;
+	}
+	return 0;
+}
