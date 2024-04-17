6d16d3d8acec50c468a3d9786b2ebef0a41a70ba
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-04-17 23:00:00 +0530

    solved 1 cakewalk problem

---
+++ 17Apr24/SugarCane_Juice_Business.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout << n*(int)(50-(50*0.70)) << endl;
+	}
+	return 0;
+}
